3d37bd138f566d4d8f8843b4e5c8375734c2f2df
Maria Civilis <maria.civilis@tulip.io>
2016-06-23 15:50:04 -0400

    Initial Commit

---
+++ JSONToObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONToObject
+//
+//  Created by Maria Civilis on 2016-06-23.
+//  Copyright © 2016 Maria Civilis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
