78e8c27ed45299de41cab725318715e8ba025bc6
Maria Civilis <maria.civilis@tulip.io>
2016-07-05 10:08:43 -0400

    cleanup

--- JSONToObject/Helpers.swift
+++ JSONToObject/Helpers.swift
@@ -9,33 +9,22 @@
 import Foundation
 
 
-//The flatten(_:) function takes a double optional and removes one level of optional-ness. The custom operator >>>= takes an optional of type A to the left, and a function that takes an A as a parameter and returns an optional B to the right.
+// The flatten(_:) function takes a double optional and removes one level of optional-ness
+// The custom operator >>>= takes an optional of type A to the left, and a function that takes an A as a parameter and returns an optional B to the right.
 
-//use my own verbose variables for clarity
-//func flatten <OptionalType> (optional: OptionalType??) -> OptionalType? {
-//    if let result = optional {
-//        return result
-//    }
-//    return nil
-//}
-
-//use my own verbose variables for clarity
-
-//infix operator >>>* {}
-//func >>>* <OptionalType, UnwrappedOptionalType> (optional: OptionalType?, performUnwrapping: OptionalType -> UnwrappedOptionalType) -> UnwrappedOptionalType? {
-//    return flatten(optional.map(performUnwrapping))
-//}
-
-func flatten<A>(x: A??) -> A? {
-    if let y = x { return y }
+func flatten <Optional> (optional: Optional??) -> Optional? {
+    if let unwrappedOptional = optional {
+        return unwrappedOptional
+    }
     return nil
 }
 
 infix operator >>>= {}
-func >>>= <A, B> (optional: A?, f: A -> B?) -> B? {
-    return flatten(optional.map(f))
+func >>>= <A, B> (optional: A?, function: A -> B?) -> B? {
+    return flatten(optional.map(function))
 }
 
+// MARK: Retrieve data from JSON structures in a type-safe manner
 func number(input: [NSObject:AnyObject], key: String) -> NSNumber? {
     return input[key] >>>= { $0 as? NSNumber }
 }
@@ -59,6 +48,3 @@ func string(input: [String:AnyObject], key: String) -> String? {
 func bool(input: [String:AnyObject], key: String) -> Bool? {
     return number(input, key: key).map { $0.boolValue }
 }
-
-
-//http://swanros.com/how-i-deal-with-json-in-swift/
